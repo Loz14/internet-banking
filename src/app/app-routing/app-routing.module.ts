960b71a0c739fcaf5befaa3cb41a65794c133004
Luiz André Carvalho <luizandre.ita@gmail.com>
2020-02-19 22:13:53 -0300

    ajudando styles e novas rotas

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { ContentComponent } from '../content/content.component';
+import { ContentComponent } from './../content/content.component';
+import { CadastroClientesComponent } from './../cadastro-clientes/cadastro-clientes.component';
+import { CadastroConcluidoComponent } from '../cadastro-concluido/cadastro-concluido.component';
+import { HomeLoginComponent } from '../home-login/home-login.component';
+import { AcessoNegadoComponent } from '../acesso-negado/acesso-negado.component';
+import { LoginComponent } from '../login/login.component';
 
 const routes: Routes = [
-  { path: 'cadastro', component: ContentComponent }
+  { path: '', component: ContentComponent },
+  { path: 'cadastro-clientes', component: CadastroClientesComponent },
+  { path: 'cadastro-concluido', component: CadastroConcluidoComponent },
+  { path: 'home-logada', component: HomeLoginComponent },
+  { path: 'acesso-negado', component: AcessoNegadoComponent },
+  { path: 'login', component: LoginComponent },
 ];
 
 @NgModule({
-  declarations: [],
   imports: [
     CommonModule,
     RouterModule.forRoot(routes)
   ],
+  declarations: [],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
