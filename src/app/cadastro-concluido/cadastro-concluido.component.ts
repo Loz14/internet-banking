8628f515d7b9b927ee952ea0195f2816fcd2fbac
Luiz André Carvalho <luizandre.ita@gmail.com>
2020-02-19 22:17:34 -0300

    ts do cadastro-concluido

--- src/app/cadastro-concluido/cadastro-concluido.component.ts
+++ src/app/cadastro-concluido/cadastro-concluido.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro-concluido',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroConcluidoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.navegarParaLogin();
   }
-
+  navegarParaLogin() {
+    setTimeout(() => {
+      this.router.navigate(['/login']);
+    }, 3000);
+  }
 }
